a40c8454c2eed7704ce8099b469381042c61b858
Jordan Leich <jordanleich@gmail.com>
2021-05-11 22:11:47 -0400

    v1.0

---
+++ game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# Made by Jordan Leich & Mordy Waldner on 5/11/2021, Last updated on 5/11/2021, Version 1.0
+
+# TODO List add a merchant, add a user currency, add a save feature
+
+# Imports
+import colors
+import json
+import time
+import random
+
+# Global variables
+user_balance = 0
+
+
+def game_intro_description():
+    print('start here, this game is about...')
+
+
+game_intro_description()
