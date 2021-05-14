9c4ad5d8a05341fbf231ed40dea77ce1c47ef50c
Jordan Leich <jordanleich@gmail.com>
2021-05-13 23:14:37 -0400

    added game description
    Signed-off-by: Jordan Leich <jordanleich@gmail.com>

--- game.py
+++ game.py
@@ -22,7 +22,10 @@ except FileNotFoundError:
 
 
 def game_intro_description():
-    print('start here, this game is about...\n')
+    print('This is a zombie survival game where you must make the best choices and decisions possible in order to live.'
+          ' As a survivor, you will encounter zombies, weapons, people, and a merchant to buy from with an '
+          'in-game currency. Every decision you make has a cause and effect while some lead you to fortune and others '
+          'lead you to death.\n')
     game()
 
 
