e07091eb36eb1c1e9a59073b0b595a0f97ab1b1e
amastis <37512516+amastis@users.noreply.github.com>
2021-10-26 02:30:16 -0400

    Update colors.py

--- other/colors.py
+++ other/colors.py
@@ -1,11 +1,24 @@
 # Created by Jordan Leich on 7/27/2020
 
-# Imports
 from colored import fg, attr
 
-# Globals
-green = fg('green')
-red = fg('red')
-yellow = fg('yellow')
-blue = fg('blue')
-reset = attr('reset')
+GREEN = fg('green')
+RED = fg('red')
+YELLOW = fg('yellow')
+BLUE = fg('blue')
+RESET = attr('reset')
+
+def print_green(message_text):
+    print(f'{GREEN}{message_text}{RESET}')
+
+def print_red(message_text):
+    print(f'{RED}{message_text}{RESET}')
+
+def print_yellow(message_text):
+    print(f'{YELLOW}{message_text}{RESET}')
+
+def print_blue(message_text):
+    print(f'{BLUE}{message_text}{RESET}')
+
+def user_error(message_text):
+    print_red(message_text)
