912dae50c08d46edde698065219e79b3cfcfe3d7
amastis <37512516+amastis@users.noreply.github.com>
2021-10-26 23:12:00 -0400

    made into class + improve random generation

--- other/sounds_effects.py
+++ other/sounds_effects.py
@@ -1,96 +1,93 @@
-from random import randint
+from random import choice
 import pygame
-from classes import Player
-from gamedata import Game_data
-from game import audio_options
 
-player1 = Player()
-game_data = Game_data()
+class Game_Sounds():
+  def __init__(self, volume):
+    self.volume_level = volume
 
-
-def _play_sound(file_path):
-    """
-Plays all the sounds given from file path
-    """
-    pygame.mixer.init()
-    soundObj = pygame.mixer.Sound(file_path)
-    soundObj.set_volume(player1.volume_level)  # Default volume is 0.05. Max volume is 1.0 and Min volume is 0.0
-    soundObj.play()
+  def _play_sound(self, file_path):
+      """
+  Plays all the sounds given from file path
+      """
+      pygame.mixer.init()
+      soundObj = pygame.mixer.Sound(file_path)
+      soundObj.set_volume(self.volume_level)  # Default volume is 0.05. Max volume is 1.0 and Min volume is 0.0
+      soundObj.play()
 
 
-def intro_sound():
-    _play_sound('sounds/introsoundtrack.mp3')
+  def intro_sound(self):
+      self._play_sound('sounds/introsoundtrack.mp3')
 
 
-def bad_ending():
-    _play_sound('sounds/dark_element_burst.wav')
+  def bad_ending(self):
+      self._play_sound('sounds/dark_element_burst.wav')
 
 
-def difficulty_select_sound():
-    _play_sound('sounds/deep_doom.wav')
+  def difficulty_select_sound(self):
+      self._play_sound('sounds/deep_doom.wav')
 
 
-def bad_luck():
-    badluck_paths = ['sounds/badluck.wav',
-                     'sounds/dark_slam.wav',
-                     'sounds/effect_horror_alerted.wav',
-                     ]
-    _play_sound(badluck_paths[randint(0, len(badluck_paths) - 1)])
+  def bad_luck(self):
+      badluck_paths = ['sounds/badluck.wav',
+                       'sounds/dark_slam.wav',
+                       'sounds/effect_horror_alerted.wav',
+                       ]
+      self._play_sound(choice(badluck_paths))
 
 
-def good_luck():
-    _play_sound('sounds/goodluck.wav')
+  def good_luck(self):
+      self._play_sound('sounds/goodluck.wav')
 
 
-def merchant_purchase_sound():
-    merchant_paths = ['sounds/cashreg.wav',
-                      'sounds/metal_shing.wav',
+  def merchant_purchase_sound(self):
+      merchant_paths = ['sounds/cashreg.wav',
+                        'sounds/metal_shing.wav',
+                        ]
+      self._play_sound(choice(merchant_paths))
+
+
+  def horror_sound_effects(self):
+      horror_paths = ['sounds/horrorsoundeffect1.mp3',
+                      'sounds/horrorsoundeffect2.mp3',
+                      'sounds/horrorsoundeffect3.mp3',
+                      'sounds/ghost_movement.wav',
+                      'sounds/supernatural.wav',
+                      'sounds/spooky_breathe_evil.wav',
+                      'sounds/spooky_ambience.wav',
                       ]
-    _play_sound(merchant_paths[randint(0, len(merchant_paths) - 1)])
+      self._play_sound(choice(horror_paths))
 
 
-def horror_sound_effects():
-    horror_paths = ['sounds/horrorsoundeffect1.mp3',
-                    'sounds/horrorsoundeffect2.mp3',
-                    'sounds/horrorsoundeffect3.mp3',
-                    'sounds/ghost_movement.wav',
-                    'sounds/supernatural.wav',
-                    'sounds/spooky_breathe_evil.wav',
-                    'sounds/spooky_ambience.wav',
-                    ]
-    _play_sound(horror_paths[randint(0, len(horror_paths) - 1)])
+  def wind_sound(self):
+      self._play_sound('sounds/heavy_wind.wav')
 
 
-def wind_sound():
-    _play_sound('sounds/heavy_wind.wav')
+  def zombie_attack_inside(self):
+      zombie_inside_path = ['sounds/zombieattack1inside.mp3',
+                            'sounds/zombieattack2inside.mp3',
+                            'sounds/zombieattack3inside.mp3',
+                            'sounds/monster_screech.wav',
+                            'sounds/breathe_ghost_eerie.wav',
+                            ]
+      self._play_sound(choice(zombie_inside_path))
 
 
-def zombie_attack_inside():
-    zombie_inside_path = ['sounds/zombieattack1inside.mp3',
-                          'sounds/zombieattack2inside.mp3',
-                          'sounds/zombieattack3inside.mp3',
-                          'sounds/monster_screech.wav',
-                          'sounds/breathe_ghost_eerie.wav',
-                          ]
-    _play_sound(zombie_inside_path[randint(0, len(zombie_inside_path) - 1)])
+  def parkview_entrance(self):
+      self._play_sound('sounds/spooky_werewolf_howl.wav')
 
 
-def parkview_entrance():
-    _play_sound('sounds/spooky_werewolf_howl.wav')
+  def climatic(self):
+      climatic_paths = ['sounds/climatic.wav',
+                        'sounds/action_riser.wav',
+                        ]
+      self._play_sound(choice(climatic_paths))
 
 
-def climatic():
-    climatic_paths = ['sounds/climatic.wav',
-                      'sounds/action_riser.wav',
-                      ]
-    _play_sound(climatic_paths[randint(0, len(climatic_paths) - 1)])
-
-
-def zombie_attack_outside():
-    zombie_outside_path = ['sounds/zombieattack1inside.mp3',
-                           'sounds/zombieattack2inside.mp3',
-                           'sounds/zombieattack3inside.mp3',
-                           'sounds/monster_ghost_death.wav',
-                           'sounds/breathe_ghost_eerie.wav',
-                           ]
-    _play_sound(zombie_outside_path[randint(0, len(zombie_outside_path) - 1)])
+  def zombie_attack_outside(self):
+      zombie_outside_path = ['sounds/zombieattack1inside.mp3',
+                             'sounds/zombieattack2inside.mp3',
+                             'sounds/zombieattack3inside.mp3',
+                             'sounds/monster_ghost_death.wav',
+                             'sounds/breathe_ghost_eerie.wav',
+                             ]
+      self._play_sound(choice(zombie_outside_path))
