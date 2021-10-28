19cd4032f352ea62014ed6fb650d91787831d460
crazyguy42781 <liversmiles@gmail.com>
2021-10-28 13:55:18 -0400

    Revert "small fixes"
    This reverts commit 3384dea6a53d6c690c12f11f30d38dd944064e45.

--- other/colors.py
+++ other/colors.py
@@ -14,27 +14,22 @@ def print_green(message_text, sleep_duration=0):
     print(f'{GREEN}{message_text}{RESET}')
     sleep(sleep_duration)
 
-
 def print_red(message_text, sleep_duration=0):
     print(f'{RED}{message_text}{RESET}')
-    sleep(sleep_duration)
-
+	sleep(sleep_duration)
 
 def print_yellow(message_text, sleep_duration=0):
     print(f'{YELLOW}{message_text}{RESET}')
     sleep(sleep_duration)
 
-
 def print_blue(message_text, sleep_duration=0):
     print(f'{BLUE}{message_text}{RESET}')
     sleep(sleep_duration)
 
-
 def user_error(message_text, sleep_duration=0):
     print_red(message_text, sleep_duration)
 
-
 # print and sleep
-def print_s(message_text, sleep_duration=0):
+def print_s(message_text, sleep_duration=0)
     print(message_text)
     sleep(sleep_duration)
