b642b4d4bce2a37dc7e1f0527b5dae67d171f120
amastis <37512516+amastis@users.noreply.github.com>
2021-10-30 12:05:09 -0400

    Update gamedata.py

--- gamedata.py
+++ gamedata.py
@@ -11,36 +11,19 @@ This class will offer loading and saving of game data for Zombie-Survival-Game.
         self.file = 'data.json'
         self.file_exists = bool(file_exists(self.file))
 
-    def load_game(self, player1: object):
+    def load_game(self):
         if self.file_exists:
             with open(self.file, 'r') as user_data_file:
                 user_data = json.load(user_data_file)
-                player1.load_data(user_data)
                 print_green('Saved data has been loaded successfully!\n')
+                return user_data
         else:  # don't need to initialize values -- already initialized when class created
             print_yellow('No saved data found...\n')
             print_green('Starting a fresh game...\n')
 
-    def save_game(self, player1: object):
+    def save_game(self, player_data: dict):
         if not self.file_exists:
             print_yellow("No previous save! Creating new save...\n")
 
         with open(self.file, 'w') as user_data_file:
-            str_2 = str(player1.difficulty).split('.')
-            if str_2[1] == 'God':
-                new_V = 0
-            elif str_2[1] == 'Easy':
-                new_V = 1
-            elif str_2[1] == 'Medium':
-                new_V = 2
-            elif str_2[1] == 'Hard':
-                new_V = 3
-
-            save_info = {
-                'balance': player1.balance,
-                'health': player1.health,
-                'difficulty': new_V,
-                'check_point': player1.check_point,
-            }
-            json.dump(save_info, user_data_file, indent=4)
-            #  json.dump(player1.get_data(), user_data_file)
+            json.dump(player_data, user_data_file)
